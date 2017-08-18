70bd112a5d0c14e0503b3ac0c53764ece5b07e67
David Eichmann <EichmannD@gmail.com>
2017-08-18 16:02:49 +0100

    Basic FRP

--- app/Main.hs
+++ app/Main.hs
@@ -1,47 +1,63 @@
+{-# LANGUAGE RecursiveDo #-}
+
 module Main where
 
+import Control.Concurrent (forkIO)
 import Data.IORef
 import Data.List (foldl')
+import Graphics.UI.WX
 import Lib
+import Safe
 import System.Console.GetOpt
 import System.Environment (getArgs)
 
-compilerOpts :: [String] -> IO (Options, [String])
-compilerOpts argv =
-  case getOpt Permute options argv of
-    (o, n, []) -> return (foldl' (flip ($)) defaultOptions o, n)
-    (_, _, errs) ->
-      ioError (userError (concat errs ++ usageInfo header options))
-  where
-    header = "Usage: [OPTION...]"
+defaultServerPort :: Int
+defaultServerPort = 10000
+
+defaultClientPort :: Int
+defaultClientPort = 10001
 
 main :: IO ()
 main
   -- Args
  = do
-  (opts, []) <- compilerOpts =<< getArgs
+  (opts, []) <- argsToOpts =<< getArgs
   -- FRP Behavior
-  inputA <- newIORef 10
-  inputB <- newIORef 20
-  let aBeh :: Behavior Int
-      aBeh = Poll (readIORef inputA)
-      bBeh :: Behavior Int
-      bBeh = Poll (readIORef inputB)
-      testBeh :: Behavior String
+  (aB, aS) <- mkSink Nothing
+  (bB, bS) <- mkSink Nothing
+  let testBeh :: Behavior String
       testBeh =
-        Lift2
-          (\a b -> show a ++ " + " ++ show b ++ " = " ++ show (a + b))
-          aBeh
-          bBeh
-  -- Actuate
-  localActuate
-    testBeh
-    (\out -> do
-       modifyIORef inputA (+ 1)
-       putStrLn out)
-  --case optionsMode opts of
-  --  Client srvPort -> netActuateClient srvPort clientBehavior
-  --  Server -> netActuateServer clientBehavior
+        Lift2 (\a b -> show a ++ " + " ++ show b ++ " = " ++ show ((+) <$> a <*> b)) aB bB
+  start $ do
+    -- Actuate
+    outCtrl <- mkGui aS bS
+    localActuate testBeh (\o -> set outCtrl [text := o])
+    --case optionsMode opts of
+    --  Client srvPort -> netActuateClient srvPort clientBehavior
+    --  Server -> netActuateServer clientBehavior
+
+mkGui :: BehaviorSink (Maybe Int) -> BehaviorSink (Maybe Int) -> IO (StaticText ())
+mkGui aS bS = mdo
+  a <- maybe "" show <$> getSink aS
+  b <- maybe "" show <$> getSink bS
+  f <- frame []
+  aCtrl <-
+    textEntry
+      f
+      [text := a, on update := (setSink aS =<< readMay <$> get aCtrl text)]
+  bCtrl <-
+    textEntry
+      f
+      [text := b, on update := (setSink bS =<< readMay <$> get bCtrl text)]
+  outText <- staticText f [text := " "]
+  set
+    f
+    [ layout := column 0 $
+      margin 10 <$>
+      [floatCenter (widget aCtrl), floatCenter (widget bCtrl), widget outText]
+    ]
+  refit f
+  return outText
 
 data Options = Options
   { optionsMode :: Mode
@@ -52,10 +68,6 @@ data Mode
   = Client Int -- ^Port of the server
   | Server
   deriving (Show)
-
-defaultServerPort = 10000
-
-defaultClientPort = 10001
 
 defaultOptions =
   Options
@@ -83,3 +95,12 @@ options =
       (ReqArg (\port opts -> opts {optionsPort = read port}) "port")
       "port"
   ]
+
+argsToOpts :: [String] -> IO (Options, [String])
+argsToOpts argv =
+  case getOpt Permute options argv of
+    (o, n, []) -> return (foldl' (flip ($)) defaultOptions o, n)
+    (_, _, errs) ->
+      ioError (userError (concat errs ++ usageInfo header options))
+  where
+    header = "Usage: [OPTION...]"
--- src/Lib.hs
+++ src/Lib.hs
@@ -2,24 +2,56 @@
 
 module Lib where
 
+import Control.Monad
+import Data.IORef
+
 data Behavior a
-  = Poll (IO a)
+  = Sink (BehaviorSink a)
   | forall b. Lift1 (b -> a)
                     (Behavior b)
-  | forall c b. Lift2 (b -> c -> a)
+  | forall b c. Lift2 (b -> c -> a)
                       (Behavior b)
                       (Behavior c)
 
+data BehaviorSink a =
+  BehaviorSink (IORef a) -- ^Value
+               (IORef [a -> IO ()]) -- ^Listeners
+
 evaluate :: Behavior a -> IO a
-evaluate (Poll poll) = poll
+evaluate (Sink (BehaviorSink v _)) = readIORef v
 evaluate (Lift1 f b) = f <$> evaluate b
 evaluate (Lift2 f b c) = f <$> evaluate b <*> evaluate c
 
-localActuate :: Behavior a -> (a -> IO ()) -> IO ()
-localActuate beh callback = do
-  callback =<< evaluate beh
-  localActuate beh callback
+mkSink :: a -> IO (Behavior a, BehaviorSink a)
+mkSink a = do
+  aIORef <- newIORef a
+  listeners <- newIORef []
+  let sink = BehaviorSink aIORef listeners
+  return (Sink sink, sink)
 
+getSink :: BehaviorSink a -> IO a
+getSink (BehaviorSink vIORef _) = readIORef vIORef
+
+setSink :: BehaviorSink a -> a -> IO ()
+setSink (BehaviorSink aIORef listenersRef) a = do
+  listeners <- readIORef listenersRef
+  writeIORef aIORef a
+  forM_ listeners ($ a)
+
+subscribeToAnyChange :: Behavior a -> IO () -> IO ()
+subscribeToAnyChange (Sink (BehaviorSink _ lsIORef)) listener =
+  modifyIORef lsIORef (const listener :)
+subscribeToAnyChange (Lift1 _ b) listener = subscribeToAnyChange b listener
+subscribeToAnyChange (Lift2 _ b c) listener = do
+  subscribeToAnyChange b listener
+  subscribeToAnyChange c listener
+
+localActuate :: Behavior a -> (a -> IO ()) -> IO ()
+localActuate b callback = do
+  let update = callback =<< evaluate b
+  subscribeToAnyChange b update
+  update
+{-
 netActuateClient :: Int -> Behavior a -> IO ()
 netActuateClient clientPort serverPort = undefined
 
@@ -31,3 +63,4 @@ clientBehavior = undefined
 
 serverBehavior :: Behavior a
 serverBehavior = undefined
+-}
