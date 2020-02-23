8e1ae58bd856b5ceb23c799598196477022c64c6
David Eichmann <EichmannD@gmail.com>
2020-02-23 16:46:25 +0000

    Split Circles in to 2 modules

--- app/Circles.hs
+++ app/Circles.hs
@@ -32,61 +32,34 @@ import           Data.IORef
 import           Data.Map (Map)
 import qualified Data.Map as Map
 import           Data.Maybe (isJust)
-import           GHC.Generics (Generic)
 import           Graphics.Gloss.Interface.IO.Game hiding (Event)
 import           System.Console.CmdArgs
 
 import           GateRep
 import           NFRP
 
+import           Circles.Game
+
 data CommandLineOpts = CommandLineOpts
-    { node :: Maybe Node
+    { node :: Maybe Player
     } deriving (Show, Data, Typeable)
 
 commandLineOpts :: CommandLineOpts
 commandLineOpts = CommandLineOpts
     { node = def
-                &= help ("One of " ++ show [minBound..(maxBound :: Node)])
+                &= help ("One of " ++ show [minBound..(maxBound :: Player)])
                 &= typ  "NODE"
     }
 
-data Node
-    = Player1
-    | Player2
-    deriving stock (Eq, Ord, Show, Bounded, Enum, Data, Generic)
-    deriving anyclass (Binary)
-
-data Game = Game
-    { player1Pos :: Pos
-    , player2Pos :: Pos
-    }
-    deriving stock (Show, Read, Generic)
-    deriving anyclass (Binary, NFData)
-
-data InputDir = DirUp | DirRight | DirDown | DirLeft
-    deriving stock (Eq, Ord, Show, Read, Generic)
-    deriving anyclass (Binary, NFData)
-
-createGame :: Map Node (Event InputDir) -> Behavior Game
-createGame inputs
-    = Game
-        <$> inputEToPos (inputs Map.! Player1)
-        <*> inputEToPos (inputs Map.! Player2)
-    where
-    inputEToPos :: Event InputDir -> Behavior Pos
-    inputEToPos inputE = step (0, 0) ((\dir -> case dir of
-                    DirLeft  -> (-delta,  0)
-                    DirRight -> ( delta,  0)
-                    DirUp    -> ( 0,  delta)
-                    DirDown  -> ( 0, -delta)
-                    ) <$> inputE)
-    delta = 100
-
+-- | Create source events for all players.
 createSourceEvents
     :: (Binary input, NFData input)
-    => Node
+    => Player
+    -- ^ Current Player.
     -> IO ( Maybe input -> IO ()
-          , Map Node (Event input)
+            -- ^ Fire my input event.
+          , Map Player (Event input)
+            -- ^ Input events of all players (including my self).
           )
 createSourceEvents myNode = do
     let localhost = "127.0.0.1"
@@ -114,10 +87,6 @@ main = do
     let myNodeIx = fromEnum myNode
         windowPos = (10 + (myNodeIx * 510),10)
 
-    --
-    -- FRP
-    --
-
     -- Inputs
     (fireInput, inputs) <- createSourceEvents myNode
     let gameB = createGame inputs
@@ -138,202 +107,20 @@ main = do
 
     -- Initialize with no events up to time 0 (TODO this should be part of a higher lever API)
     playIO
-        (InWindow "NFRP Demo" (500, 500) windowPos)
-        black
-        60
-        ()
+        (InWindow "NFRP Demo" (500, 500) windowPos) black 60 ()
         -- Draw The Game
-        (\ () -> do
-            game <- getGame
-            return $ Pictures
-                [ drawCharacter red  (player1Pos game)
-                , drawCharacter blue (player2Pos game)
-                ]
-        )
-        -- Fire Input Events
+        (\ () -> drawGame <$> getGame)
+        -- Input Handler
         (\ event () -> do
             -- Get inputs.
-            let inputsMay = case event of
-                    EventKey (SpecialKey sk) Down _modifiers _mousePos
-                        -> case sk of
-                            KeyUp    -> Just DirUp
-                            KeyRight -> Just DirRight
-                            KeyDown  -> Just DirDown
-                            KeyLeft  -> Just DirLeft
-                            _        -> Nothing
-                    _   -> Nothing
+            let inputsMay = getInputsMay event
 
             -- We let the "World Update" function below fire the Nothing case.
             when (isJust inputsMay) (fireInput inputsMay)
         )
-        -- Note we need to call fireInput here to make sure we keep other nodes
-        -- up to date even when no input events are occuring.
+        -- "World Update"
+        -- Note we need to call fireInput here to progress time.
         (\ _dt () -> fireInput Nothing)
 
 drawCharacter :: Color -> Pos -> Picture
 drawCharacter c (x, y) = Color c (translate x y (Circle 10))
-
-type Pos = (Float, Float)
-
-{-}
--- Lets make a simple calculator example with 3 clients and a server that we want to do that calculating.
-data Node
-    = Player
-    | Bot
-    deriving (Show, Read, Eq, Ord, Bounded, Enum)
-
-instance Sing Player where
-    sing _ = Player
-instance Sing Bot where
-    sing _ = Bot
-
-
-data Ctx = Ctx (IORef Pos) (IORef Pos)
-
-type MT = MomentTypes Node Ctx
-
-type Mom a = Moment MT a
-
-allNodes :: [Node]
-allNodes = [minBound..maxBound]
-
-newCtx :: IO Ctx
-newCtx = Ctx
-    <$> newIORef (0,0)
-    <*> newIORef (0,0)
-
-main :: IO ()
-main = do
-    playerCtx <- newCtx
-    botCtx    <- newCtx
-    let nodeCtxs = [ NodeCtx Player playerCtx
-                   , NodeCtx Bot    botCtx
-                   ]
-
-    -- Start simulation
-    (stop, circOuts, injectors, clocks) <- simulate
-        circuit
-        nodeCtxs
-        Bot
-
-    -- Start Bot GUI
-    let botInjector = injectors Map.! Bot
-        botSrcEvt = botInputDirSrcEvt $ circOuts Map.! Bot
-
-        botAI = do
-            injectEvent botInjector botSrcEvt DirUp
-            threadDelay 1000000
-            injectEvent botInjector botSrcEvt DirRight
-            threadDelay 1000000
-            injectEvent botInjector botSrcEvt DirDown
-            threadDelay 1000000
-            injectEvent botInjector botSrcEvt DirLeft
-            threadDelay 1000000
-            botAI
-
-    _ <- forkIO botAI
-
-    -- Start Player GUI
-    playerGUI
-        (700, 100)
-        playerCtx
-        (playerInputDirSrcEvt $ circOuts Map.! Player)
-        (injectors Map.! Player)
-        (clocks Map.! Player)
-
-    --Stop
-    stop
-
-data CircOut = CircOut
-    { playerInputDirSrcEvt :: SourceEvent Node InputDir -- Player
-    , botInputDirSrcEvt    :: SourceEvent Node InputDir -- Bot
-    }
-
-playerGUI :: (Eq node, Show node)
-          => (Int, Int)
-          -> Ctx
-          -> SourceEvent node InputDir
-          -> EventInjector node
-          -> IO Time
-          -> IO ()
-playerGUI windowPos (Ctx pPosIORef bPosIORef) inputDirSourceE injector _getTime = playIO
-    (InWindow "NFRP Demo" (500, 500) windowPos)
-    black
-    60
-    ()
-    (\ () -> do
-        playerPos <- readIORef pPosIORef
-        botPos    <- readIORef bPosIORef
-        return $ Pictures
-            [ drawCharacter red  playerPos
-            , drawCharacter blue botPos
-            ]
-    )
-    (\ event () -> do
-        case event of
-            EventKey (SpecialKey sk) Down _modifiers _mousePos
-                -> maybe (return ()) (injectEvent injector inputDirSourceE) $ case sk of
-                    KeyUp    -> Just DirUp
-                    KeyRight -> Just DirRight
-                    KeyDown  -> Just DirDown
-                    KeyLeft  -> Just DirLeft
-                    _        -> Nothing
-            _   -> return ()
-        return ()
-        )
-    (\ _dt () ->
-        -- TODO step world... do we need to do this?
-        return ()
-        )
-
-circuit :: Mom CircOut
-circuit = do
-    (playerDirSE, playerDirE) <- newSourceEvent Player
-    (botDirSE   , botDirE   ) <- newSourceEvent Bot
-
-    playerPosB
-        <- beh
-        . sendBAll Player
-        . step (0,0)
-        $ dirToPos <$> playerDirE
-
-    botPosB
-        <- beh
-        . sendBAll Bot
-        . step (0,0)
-        $ dirToPos <$> botDirE
-
-    pDir  <- beh $ step  DirUp playerDirE
-    pDirD <- beh $ delay DirUp pDir
-    pDirBothB    <- beh $ (,) <$> pDir <*> pDirD
-    pDirBothOnEB <- beh
-                    . step Nothing
-                    $ sample (\ valB _valE -> Just valB) pDirBothB playerDirE
-
-    listenB Player pDirBothB    (\ _ a _ -> putStrLn $ "@@@ Expecting same: " ++ show a)
-    listenB Player pDirBothOnEB (\ _ a _ -> putStrLn $ "@@@ Expecting diff: " ++ show a)
-
-    let bind ::Node -> Mom ()
-        bind node = do
-            listenB node playerPosB (\ (Ctx ref _) pos _ -> writeIORef ref pos)
-            listenB node botPosB    (\ (Ctx _ ref) pos _ -> writeIORef ref pos)
-
-    bind Player
-    bind Bot
-
-    return (CircOut playerDirSE botDirSE)
-
-dirToPos :: InputDir -> Pos
-dirToPos DirUp    = (0, 20)
-dirToPos DirRight = (20, 0)
-dirToPos DirDown  = (0, -20)
-dirToPos DirLeft  = (-20, 0)
-
--- movementControler :: EventIx os (InputDir, KeyState) -> Mom (BehaviorIx os (Time -> Pos))
--- movementControler inDirE = do
---     activeDirsB :: BehaviorIx os [InputDir]
---         <- (beh $ Step []) <$> fmap (\ () -> _) inDirE
-
---     _
-
--}
---
+++ app/Circles/Game.hs
@@ -0,0 +1,99 @@
+{-# OPTIONS_GHC -Wno-unticked-promoted-constructors #-}
+
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE PartialTypeSignatures #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeInType #-}
+
+module Circles.Game where
+
+import           Control.DeepSeq
+import           Data.Binary (Binary)
+import           Data.Data
+import           Data.Map (Map)
+import qualified Data.Map as Map
+import           GHC.Generics (Generic)
+import           Graphics.Gloss.Interface.IO.Game hiding (Event)
+import qualified Graphics.Gloss.Interface.IO.Game as Gloss
+
+import           GateRep
+
+data Player
+    = Player1
+    | Player2
+    deriving stock (Eq, Ord, Show, Bounded, Enum, Data, Generic)
+    deriving anyclass (Binary)
+
+type Pos = (Float, Float)
+
+data Game = Game
+    { player1Pos :: Pos
+    , player2Pos :: Pos
+    }
+    deriving stock (Show, Read, Generic)
+    deriving anyclass (Binary, NFData)
+
+data Inputs = DirUp | DirRight | DirDown | DirLeft
+    deriving stock (Eq, Ord, Show, Read, Generic)
+    deriving anyclass (Binary, NFData)
+
+-- | Game Logic
+createGame
+    :: Map Player (Event Inputs)
+    -- ^ Input's of all players.
+    -> Behavior Game
+    -- ^ Game state.
+createGame inputs
+    = Game
+        <$> inputEToPos (inputs Map.! Player1)
+        <*> inputEToPos (inputs Map.! Player2)
+    where
+    inputEToPos :: Event Inputs -> Behavior Pos
+    inputEToPos inputE = step (0, 0) ((\dir -> case dir of
+                    DirLeft  -> (-delta,  0)
+                    DirRight -> ( delta,  0)
+                    DirUp    -> ( 0,  delta)
+                    DirDown  -> ( 0, -delta)
+                    ) <$> inputE)
+    delta = 100
+
+-- | Get the inputs form a Gloss Event.
+getInputsMay :: Gloss.Event -> Maybe Inputs
+getInputsMay event = case event of
+    EventKey (SpecialKey sk) Down _modifiers _mousePos
+        -> case sk of
+            KeyUp    -> Just DirUp
+            KeyRight -> Just DirRight
+            KeyDown  -> Just DirDown
+            KeyLeft  -> Just DirLeft
+            _        -> Nothing
+    _   -> Nothing
+
+-- | Draw the game with Gloss.
+drawGame :: Game -> Picture
+drawGame (Game player1Pos' player2Pos')
+    = Pictures
+        [ drawCharacter red  10 player1Pos'
+        , drawCharacter blue 20 player2Pos'
+        ]
+    where
+    drawCharacter :: Color -> Float -> Pos -> Picture
+    drawCharacter c size (x, y) = Color c (translate x y (thickCircle size 6))
